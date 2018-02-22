324822923d1e635c07dfb815d38923626176a859
Lukasz Sack <lsack@squiz.pl>
2018-02-22 10:20:53 +0100

    Ready for Tesco fun

--- scraplogin.py
+++ scraplogin.py
@@ -7,25 +7,25 @@ def main():
 
     sesssion_requests = requests.session()
 
-    login_url = "LOGIN PAGE URL"
+    login_url = "https://ezakupy.tesco.pl/groceries/pl-PL/login"
     result = sesssion_requests.get(login_url)
 
     tree = html.fromstring(result.text)
-    authenticity_token = list(set(tree.xpath("//input[@name='authenticity_token']/@value")))[0] # XPATH of hidden token
+    authenticity_token = list(set(tree.xpath("//input[@name='_csrf']/@value")))[0] # XPATH of hidden token
 
     payload = {
-        "NAME TAG FOR LOGIN": "<USERNAME>",
-        "NAME TAG FOR PASSWORD": "<PASSWORD>",
-        "NAME OF THE TOKEN e.g : authenticity_token": "<TOKEN CODE>"
+        "email": "<USERNAME>",
+        "password": "<PASS>",
+        "_csrf": "<f2Ybad5q-89AHsTYLqQXuQMUk4ybo2sC0o9A>"
     }
 
     result = sesssion_requests.post(login_url, data=payload, headers=dict(referer=login_url))
 
-    url = "URL TO SCRAP INFO FROM"
+    url = "https://ezakupy.tesco.pl/groceries/pl-PL/shop/napoje/napoje-gazowane/cola"
     result = sesssion_requests.get(url, headers=dict(referer=url))
 
     tree = html.fromstring(result.content)
-    names = tree.xpath("XPATH TO INFORMATION TO SCRAP")
+    names = tree.xpath("//*[@id='product-list']/div[2]/div[4]/div[1]/div[2]/div/div/div/ul/li/div/div/div/div/div[1]/div/a/text()")
 
     print(names)
 
