1bf9b87981344a8c9618546c861c357bb988c994
Lukasz Sack <lsack@squiz.pl>
2018-02-22 13:37:56 +0100

    Minor changes, token is now scraped from html,

--- scraplogin.py
+++ scraplogin.py
@@ -1,33 +1,36 @@
 import requests
 from lxml import html
+import logging
 
-
+login_url = "https://ezakupy.tesco.pl/groceries/pl-PL/login"
+URL = "https://ezakupy.tesco.pl/groceries/pl-PL/shop/napoje/napoje-gazowane/cola"
 
 def main():
+    sesssion_requests = requests.Session()
 
-    sesssion_requests = requests.session()
-
-    login_url = "https://ezakupy.tesco.pl/groceries/pl-PL/login"
     result = sesssion_requests.get(login_url)
 
     tree = html.fromstring(result.text)
-    authenticity_token = list(set(tree.xpath("//input[@name='_csrf']/@value")))[0] # XPATH of hidden token
-
+    authenticity_token = list(set(tree.xpath("//input[@name='_csrf']/@value")))[0]
     payload = {
-        "email": "<USERNAME>",
-        "password": "<PASS>",
-        "_csrf": "<f2Ybad5q-89AHsTYLqQXuQMUk4ybo2sC0o9A>"
+        "email": "mail",
+        "password": "pass",
+        "_csrf": authenticity_token
     }
 
-    result = sesssion_requests.post(login_url, data=payload, headers=dict(referer=login_url))
+    sesssion_requests.post(login_url, data=payload, headers=dict(referer=login_url))
 
-    url = "https://ezakupy.tesco.pl/groceries/pl-PL/shop/napoje/napoje-gazowane/cola"
-    result = sesssion_requests.get(url, headers=dict(referer=url))
+    result = sesssion_requests.get(URL, headers=dict(referer=URL))
 
     tree = html.fromstring(result.content)
-    names = tree.xpath("//*[@id='product-list']/div[2]/div[4]/div[1]/div[2]/div/div/div/ul/li/div/div/div/div/div[1]/div/a/text()")
-
+    names = tree.xpath("//*[@id='content']/div/div/div[1]/div[1]/ul/li[1]/div/text()")
     print(names)
 
+
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(
+        filename='login.log',
+        level=logging.DEBUG,
+        format="%(asctime)s:%(levelname)s: %(message)s"
+    )
+    logging.debug(main())
